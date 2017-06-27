c730351d384ed48f3b753f2534d2cbf79c06ccc7
Hyunju Kim <hkim78@asu.edu>
2017-06-27 16:46:47 -0700

    modified topology.py

--- mgmnet/topology.py
+++ mgmnet/topology.py
@@ -3,66 +3,34 @@ import numpy as np
 import kegg as kg
 import bionets as bn
 import igraph as ig
+import networkx as nx
 
 import time
 
-
-start_time = time.time() # remove it
 kegg = kg.Kegg()
-print "time to load kegg"
-print time.time() - start_time
 
-start_time = time.time() # remove it
+
 rxn_list = bn.loadRxns('ecosystem', 'YNP', 3)
-print "time to load rxn info"
-print time.time() - start_time
-
-start_time = time.time() # remove it
 se = bn.subEdges(rxn_list, kegg.rxn_reac, kegg.rxn_prod)
-print "time to generate edge list"
-print time.time() - start_time
 
-start_time = time.time() # remove it
+#igraph
+
+## graph object
 G = ig.Graph.TupleList(directed = False, edges = se)
-print "time to load edge to network object"
-print time.time() - start_time
-
-start_time = time.time() # remove it
+## largest component
 cl = G.clusters()
 lcc = cl.giant()
-print 'Time for largest connected component', time.time() - start_time
-print lcc.vcount()
-
-
-start_time = time.time()
+## average shortest length
 ave_spl_lcc = lcc.average_path_length(directed=False, unconn=True)
-print 'Time for ave_shortest_length', time.time() - start_time
-print "ave_spl_lcc = ", ave_spl_lcc
-
-start_time = time.time()
+## average clustering coefficient
 ave_cc_lcc = lcc.transitivity_undirected()
-print 'Time for ave_clustering_coefficient', time.time() - start_time
-print "ave_cc_lcc = ", ave_cc_lcc
-
-start_time = time.time()
+print ave_cc_lcc
 list_local_cc_lcc = lcc.transitivity_local_undirected(mode='zero')
-# print 'Time for list of local_clustering_coefficient', time.time() - start_time
-# print list_local_cc_lcc
-#
-# start_time = time.time()
-# list_local_computed_cc_lcc = []
-# #print list_local_cc_lcc
-# for n in list_local_cc_lcc:
-#     if str(n) != 'nan':
-#         list_local_computed_cc_lcc.append(n)
-# print "length", len(list_local_computed_cc_lcc)
-
-
-
-
 ave_cc_lcc_from_local = np.mean(list_local_cc_lcc)
-print 'Time for ave_clustering_coefficient by computing the mean of list of local cc', time.time() - start_time
-print "ave_cc_lcc from local = ", ave_cc_lcc_from_local
+print ave_cc_lcc_from_local
+
+
+
 
 
 
