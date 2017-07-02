45372cf9407757fcd6a9d4b044c1be8d1a27c0fb
Hyunju Kim <hkim78@asu.edu>
2017-07-02 14:38:49 -0700

    update topology and bio_nets

--- mgmnet/bio_nets.py
+++ mgmnet/bio_nets.py
@@ -1,20 +1,7 @@
 
-def load_list_rxn(level, group, species):
+def load_list_rxn(system_name, species):
     rxn_list = []
-    inputfile = open('../data/rxn_lists/%s_%s/%d.dat'%(level, group, species), 'r')
-    species_name = inputfile.readline().rstrip()
-    for line in inputfile:
-        rxn = line.rstrip()
-        if rxn in rxn_list:
-            continue
-        rxn_list.append(rxn)
-    inputfile.close()
-    return rxn_list
-
-
-def load_list_rxn_from_files(file_name):
-    rxn_list = []
-    inputfile = open(file_name, 'r')
+    inputfile = open('../data/rxn_lists/%s/%d.dat'%(system_name, species), 'r')
     species_name = inputfile.readline().rstrip()[2:]
     for line in inputfile:
         rxn = line.rstrip()
@@ -23,6 +10,39 @@ def load_list_rxn_from_files(file_name):
         rxn_list.append(rxn)
     inputfile.close()
     return rxn_list, species_name
+
+
+# def load_list_rxn_from_files(file_name):
+#     rxn_list = []
+#     inputfile = open(file_name, 'r')
+#     species_name = inputfile.readline().rstrip()[2:]
+#     for line in inputfile:
+#         rxn = line.rstrip()
+#         if rxn in rxn_list:
+#             continue
+#         rxn_list.append(rxn)
+#     inputfile.close()
+#     return rxn_list, species_name
+
+
+def enz_presence(system_name, nbr_species, enz):
+    dict_species_enzPresence = {}
+    inputfile = open('../data/ec_array/ec_%s.dat'%(system_name), 'r')
+    list_system_ec = inputfile.readline().rstrip().split('\t')
+    if enz in list_system_ec:
+        index_enz = list_system_ec.index(enz) # list_system_ec has unique EC numbers. In general, index() returns the first index
+        species = 0
+        for line in inputfile:
+            items = line.rstrip().split('\t')
+            species += 1
+            if items[index_enz] == '0':
+                dict_species_enzPresence[species] = 0
+            else:
+                dict_species_enzPresence[species] = 1
+    else:
+        for i in range(1, nbr_species + 1 ):
+            dict_species_enzPresence[i] = 0
+    return dict_species_enzPresence
 
 
 def rxn_edges(rxn_list, rxn_reac, rxn_prod):
@@ -56,4 +76,4 @@ if __name__=='__main__':
 
     BIOSYSTEMS = {'ecosystem_YNP':26, 'ecosystem_JGI':5587, \
             'individual_archaea':845, 'individual_bacteria':21637, \
-            'individual_archaea_parsed':199, 'individual_bacteria_parsed':1153}
+            'individual_archaea-parsed':199, 'individual_bacteria-parsed':1153}
--- mgmnet/topology.py
+++ mgmnet/topology.py
@@ -3,54 +3,53 @@ import kegg as kg
 import bio_nets as bn
 import networkx as nx
 import numpy as np
-import time
+import csv
 
 
 kegg = kg.Kegg()
 
 BIOSYSTEMS = {'ecosystem_YNP':26, 'ecosystem_JGI':5587, \
         'individual_archaea':845, 'individual_bacteria':21637, \
-        'individual_archaea_parsed':199, 'individual_bacteria_parsed':1153}
+        'individual_archaea-parsed':199, 'individual_bacteria-parsed':1153}
 
 dir_results = '../results'
 if not os.path.exists(dir_results):
     os.makedirs(dir_results)
 
-name_lists = ['ecosystem_YNP']
-for name in name_lists:
+#name_lists = ['ecosystem_YNP', 'ecosystem_JGI']
+name_lists = ['individual_bacteria']
 
-    outputFileName = dir_results + '/%s.dat'%(name)
-    outputFile = open(outputFileName, 'w')
+
+for system_name in name_lists:
+
     header = [ "level", "group", "species", "species_name", \
                "nbr_nodes", "nbr_edges", "nbr_connected_components", \
                "nbr_nodes_lcc", "nbr_edges_lcc", \
                "ave_degree_lcc", "std_degree_lcc", \
-               "ave_degree^2_lcc", "std_degree^2_lcc", \
+               "ave_degree_square_lcc", "std_degree_square_lcc", \
                "ave_clustering_coeff_lcc", "std_clustering_coeff_lcc", \
                "ave_shortest_path_length_lcc", "std_shortest_path_length_lcc", \
-               "ave_betweeness_nodes_lcc", "std_betweeness_nodes_lcc", \
-               "ave_betweeness_edges_lcc", "std_betweeness_edges_lcc", \
+               "ave_betweenness_nodes_lcc", "std_betweenness_nodes_lcc", \
+               "ave_betweenness_edges_lcc", "std_betweenness_edges_lcc", \
                "assortativity_lcc", "attribute_assortativity_lcc", \
                "diameter_lcc", "EC 1.9.3.1 presence"]
 
-    '''
-               "ave_shortest_path_length_lcc", "std_shortest_path_length_lcc", \
-               "ave_betweeness_nodes_lcc", "std_betweeness_nodes_lcc", \
-               "ave_betweeness_edges_lcc", "std_betweeness_edges_lcc", \
-               "assortativity_lcc", "attribute_assortativity_lcc", \
-               "diameter_lcc", "EC 1.9.3.1 presence"
-    '''
-    for h in header:
-        outputFile.write('%s\t'%(h))
+    outputFileName = dir_results + '/%s.csv'%(system_name)
+    with open(outputFileName, 'w') as f:
+        csvf = csv.writer(f)
+        csvf.writerow(header)
 
     # level # group
-    [level, group] = name.split('_')
+    [level, group] = system_name.split('_')
+    # EC 1.9.3.1
+    dict_species_enzPresence = bn.enz_presence(system_name, BIOSYSTEMS[system_name], '1.9.3.1')
 
-    for species in range(BIOSYSTEMS[name]):
-        # species
-        inputFileName = '../data/rxn_lists/%s/%d.dat'%(name, species+1)
-        # species_name
-        rxn_list, species_name = bn.load_list_rxn_from_files(inputFileName)
+
+    for species in range(1, BIOSYSTEMS[system_name] + 1):
+        if species > 1:
+            continue
+        # species # species_name
+        rxn_list, species_name = bn.load_list_rxn(system_name, species)
 
         #--- Import sub-netwroks ---#
         sEdges = bn.sub_edges(rxn_list, kegg.rxn_reac, kegg.rxn_prod)
@@ -79,7 +78,7 @@ for name in name_lists:
         ave_degree_lcc = np.mean(list_degree_lcc)
         std_degree_lcc = np.std(list_degree_lcc)
 
-        # ave_degree^2_lcc # std_degree^2_lcc
+        # ave_degree_square_lcc # std_degree_square_lcc
         list_degree_square_lcc = [d^2 for d in list_degree_lcc]
         ave_degree_square_lcc = np.mean(list_degree_square_lcc)
         std_degree_square_lcc = np.std(list_degree_square_lcc)
@@ -87,11 +86,57 @@ for name in name_lists:
         # ave_clustering_coeff_lcc # std_clustering_coeff_lcc
         list_clustering_coeff_lcc = nx.clustering(G_lcc).values()
         ave_clustering_coeff_lcc = np.mean(list_clustering_coeff_lcc)
-        std_clustering_coeff_lcc = np.mean(list_clustering_coeff_lcc)
+        std_clustering_coeff_lcc = np.std(list_clustering_coeff_lcc)
+
+        # ave_shortest_path_length_lcc # std_shortest_path_length_lcc
+        dict_shortest_path_length_lcc = nx.shortest_path_length(G_lcc)
+        list_shortest_path_length_lcc = [dict_shortest_path_length_lcc[u][v] for u in G_lcc.nodes_iter() for v in G_lcc.nodes_iter()]
+        ave_shortest_path_length_lcc = np.mean(list_shortest_path_length_lcc)
+        std_shortest_path_length_lcc = np.std(list_shortest_path_length_lcc)
+
+        # ave_betweenness_nodes_lcc # std_betweenness_nodes_lcc
+        list_betweenness_nodes_lcc = nx.betweenness_centrality(G_lcc).values()
+        ave_betweenness_nodes_lcc = np.mean(list_betweenness_nodes_lcc)
+        std_betweenness_nodes_lcc = np.std(list_betweenness_nodes_lcc)
+
+        # ave_betweenness_edges_lcc # std_betweenness_edges_lcc
+        list_betweenness_edges_lcc = nx.edge_betweenness_centrality(G_lcc).values()
+        ave_betweenness_edges_lcc = np.mean(list_betweenness_edges_lcc)
+        std_betweenness_edges_lcc = np.std(list_betweenness_edges_lcc)
+
+        # assortativity_lcc
+        assortativity_lcc = nx.degree_assortativity_coefficient(G_lcc)
+
+        # attribute_assortativity_lcc
+        rEdges = bn.rxn_edges(rxn_list, kegg.rxn_reac, kegg.rxn_prod)
+        rxnG = nx.DiGraph(rEdges)
+        rxn_degree = rxnG.degree()
+        for u in G_lcc.nodes_iter():
+            G_lcc.node[u]['nbr_rxns'] = rxn_degree[u]
+        attribute_assortativity_lcc = nx.attribute_assortativity_coefficient(G_lcc, 'nbr_rxns')
+
+        # diameter_lcc
+        diameter_lcc = max(list_shortest_path_length_lcc)
+
+        # EC 1.9.3.1 presence
+        ec_presence = dict_species_enzPresence[species]
 
 
+        data = [ level, group, species, species_name, \
+                   nbr_nodes, nbr_edges, nbr_connected_components, \
+                   nbr_nodes_lcc, nbr_edges_lcc, \
+                   ave_degree_lcc, std_degree_lcc, \
+                   ave_degree_square_lcc, std_degree_square_lcc, \
+                   ave_clustering_coeff_lcc, std_clustering_coeff_lcc, \
+                   ave_shortest_path_length_lcc, std_shortest_path_length_lcc, \
+                   ave_betweenness_nodes_lcc, std_betweenness_nodes_lcc, \
+                   ave_betweenness_edges_lcc, std_betweenness_edges_lcc, \
+                   assortativity_lcc, attribute_assortativity_lcc, \
+                   diameter_lcc, ec_presence]
 
-
+        with open(outputFileName, 'a') as f:
+            csvf = csv.writer(f)
+            csvf.writerow(data)
 
 
 
@@ -101,14 +146,3 @@ for name in name_lists:
         etime = time.time()
         print nbr_nodes, "time node nbr from number_of_nodes()", etime - stime
         '''
-
-
-        rEdges = bn.rxn_edges(rxn_list, kegg.rxn_reac, kegg.rxn_prod)
-        rxnG = nx.DiGraph(rEdges)
-
-        data = [level, group, species, species_name]
-        outputFile.write('\n')
-        for d in data:
-            outputFile.write('%s\t'%(d))
-
-    outputFile.close()
