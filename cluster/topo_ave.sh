5d7021894dcc575c0ea7f9693a93399017d7a1d9
Hyunju Kim <hkim78@asu.edu>
2017-08-30 03:31:03 -0700

    Changed path for error in topo_ave.sh

--- cluster/topo_ave.sh
+++ cluster/topo_ave.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #SBATCH -n 1                        # number of cores
 #SBATCH -t 5-2:00                   # wall time (D-HH:MM)
-#SBATCH -o log/slurm.%j.out             # STDOUT (%j = JobId)
-#SBATCH -e log/slurm.%j.err             # STDERR (%j = JobId)
+#SBATCH -o out/slurm.%j.out             # STDOUT (%j = JobId)
+#SBATCH -e error/slurm.%j.err             # STDERR (%j = JobId)
 #SBATCH --mail-type=END,FAIL        # notifications for job done & fail
 #SBATCH --mail-user=hyunju.kim@asu.edu # send-to address
 
