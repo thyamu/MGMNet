ee47c1526e703be19c98868ea33a60f6a87b6ecb
Hyunju Kim <hkim78@asu.edu>
2017-09-06 11:40:40 -0700

    Updated collecting_topo_ave and Created a script to collect missing samples for deg_dist

--- code/missing_deg_dist.py
+++ code/missing_deg_dist.py
@@ -5,14 +5,8 @@ import mgmnet.bio_nets as bn
 import mgmnet.union_nets as un
 import mgmnet.syn_nets as sn
 import mgmnet.ranRxn_nets as rn
-import mgmnet.kegg_nets as kn
-import mgmnet.topo_measure as tm
 import shutil
 import pandas as pd
-
-#---------- results to be connected ==> topo_ave ----------#
-topo = tm.topoMeasure()
-header = topo.header
 
 #---------- class of networks ----------#
 module_name = sys.argv[1]
@@ -24,56 +18,39 @@ module_dict = {'bio': bn.bio(), \
 class_name = module_dict[module_name]
 
 #---------- directories ----------#
-dr_collection = ''
-for ds in ('../results_cluster', '/topo_ave', '/%s'%(module_name)):
-    dr_collection = dr_collection + ds
-    if not os.path.exists(dr_collection):
-        os.makedirs(dr_collection)
-
 dr_missing = ''
-for ds in ('../results_cluster', '/topo_ave', '/%s'%(module_name)):
+for ds in ('../results_cluster', '/deg_dist', '/%s'%(module_name)):
     dr_missing = dr_missing + ds
     if not os.path.exists(dr_missing):
         os.makedirs(dr_missing)
 
 dr_missing_batch = ''
-for ds in ('../cluster', '/missing', '/topo_ave'):
+for ds in ('../cluster', '/missing', '/deg_dist'):
     dr_missing_batch = dr_missing_batch + ds
 
 
-#---------- collecting results and identifying missing results ----------#
-df_list = []
+#---------- collecting the list of missing samples----------#
 for system_name in class_name.number_of_species.iterkeys():
-# for system_name in ['syn_individual_archaea',\
-#                     'syn_individual_bacteria',\
-#                     'syn_individual_eukarya',\
-#                     'syn_individual_all',\
-#                     'syn_ecosystem_JGI']:
     print system_name
 
     dr_results = ''
-    for ds in ('../results_cluster', '/topo_ave', '/%s'%(module_name), '/%s'%(system_name)):
+    for ds in ('../results_cluster', '/deg_dist', '/%s'%(module_name), '/sub_degree', '/%s'%(system_name)):
         dr_results = dr_results + ds
         if not os.path.exists(dr_results):
             os.makedirs(dr_results)
 
-    collectedFileName = dr_collection + '/topo_ave_%s.csv'%(system_name)
-    with open(collectedFileName, "w") as of:
-        cof = csv.writer(of)
-        cof.writerow(header)
-
-    missingFileName = dr_missing + '/missing_topo_ave_%s'%(system_name)
+    missingFileName = dr_missing + '/missing_deg_dist_%s'%(system_name)
     with open(missingFileName, "w") as mf:
        cmf = csv.writer(mf)
 
     for species in range(1, class_name.number_of_species[system_name] + 1):
         print species
         result_file_string = {'bio': '%s-%d.csv'%(system_name, species), \
-                       'union': '%s-upto-%d.csv'%(system_name, species), \
+                       'union': '%s-%d.csv'%(system_name, species), \
                        'syn': '%s-%d.csv'%(system_name, species), \
                        'ranRxn': '%s-%d.csv'%(system_name, species)} #==>change the string after generate syn results
 
-        resultFileName = dr_results + '/' + result_file_string[module_name]
+        resultFileName = dr_results + '/deg_dist_lcc_' + result_file_string[module_name]
         #print resultFileName
         if not os.path.isfile(resultFileName):
             sample = [species]
@@ -87,23 +64,20 @@ for system_name in class_name.number_of_species.iterkeys():
             data = list(crf)
             row_count = len(data)
 
-        if row_count == class_name.lines_in_topo_ave[system_name]:
-            l = class_name.lines_in_topo_ave[system_name] - 1
-            outcome = data[l]
-        else:
+        if row_count < 2:
             sample = [species]
             with open(missingFileName, "a") as mf:
                 cmf = csv.writer(mf)
                 cmf.writerow(sample)
-            continue
 
-        with open(collectedFileName, "a") as of:
-            cof = csv.writer(of)
-            cof.writerow(outcome)
 
-    df_list.append(pd.read_csv(collectedFileName))
+    #     with open(collectedFileName, "a") as of:
+    #         cof = csv.writer(of)
+    #         cof.writerow(outcome)
+    #
+    # df_list.append(pd.read_csv(collectedFileName))
 
-    missingBatchName = dr_missing_batch + '/missing_topo_ave_%s'%(system_name)
+    missingBatchName = dr_missing_batch + '/missing_deg_dist_%s'%(system_name)
     if os.path.isfile(missingBatchName):
         os.remove(missingBatchName)
 
@@ -115,7 +89,7 @@ for system_name in class_name.number_of_species.iterkeys():
     else:
         os.remove(missingFileName)
 
-#-------- Merge all files into one-----------#
-finalFileName = dr_collection + '/topo_ave_%s.csv'%(module_name)
-full_df = pd.concat(df_list)
-full_df.to_csv(finalFileName)
+# #-------- Merge all files into one-----------#
+# finalFileName = dr_collection + '/topo_ave_%s.csv'%(module_name)
+# full_df = pd.concat(df_list)
+# full_df.to_csv(finalFileName)
