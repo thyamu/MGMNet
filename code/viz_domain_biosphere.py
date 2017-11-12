2d049caaedf12608649e9920860fccc28bbe0b05
Hyunju Kim <hkim78@asu.edu>
2017-11-11 23:14:02 -0700

    add network visulization code

--- code/viz_domain_biosphere.py
+++ code/viz_domain_biosphere.py
@@ -26,6 +26,9 @@ bio = bn.bio()
 ### (2) merged net of 1, 2, and 3
 ### (3) add informtion of net 1, 2 and 3 to the merged net in (2)
 
+
+
+#-------- BEGIN: Sphere of archaea, bacteria, and eukarya and Total Ecosystem --------#
 G = {}
 union = un.union()
 for system_name in union.number_of_species.iterkeys():
@@ -34,7 +37,11 @@ for system_name in union.number_of_species.iterkeys():
     file_name = '../viz/sub_net_%s.gexf'%(system_name)
     G[system_name] = nx.Graph(sEdges)
     nx.write_gexf(G[system_name], file_name)
+#-------- END: Sphere of archaea, bacteria, and eukarya and Total Ecosystem --------#
 
+
+
+#-------- BEGIN: a  network for all domain including archaea, bacteria, and eukarya as a network attribute --------#
 list_domain = ['union_individual_archaea', \
             'union_individual_bacteria', \
             'union_individual_eukarya']
@@ -44,7 +51,7 @@ for i, j in G_all.edges_iter():
     edge_weight = 1
     for system_name in list_domain:
         if (i, j) in G[system_name]:
-            edge_weight += 1
+            edge_weight += 0.1
     G_all.add_edge(i, j, weight=edge_weight)
 
 list_node_weight = {'union_individual_archaea': 1, 'union_individual_bacteria': 10, 'union_individual_eukarya': 100}
@@ -56,6 +63,57 @@ for n in G_all.nodes_iter():
             node_weight += list_node_weight[system_name]
     G_all.add_node(n, weight=node_weight)
 
-
 file_name = '../viz/sub_net_weighted_all_domain.gexf'
 nx.write_gexf(G_all, file_name)
+#-------- END: a network for all domain including archaea, bacteria, and eukarya as a network attribute --------#
+
+
+
+#-------- BEGIN: small kegg network including archaea, bacteria, and eukarya as a network attribute --------#
+bio = bn.bio()
+sEdges = bio.sub_edges('biosphere_kegg', 2)
+G_kegg = nx.Graph(sEdges)
+
+for n in G_kegg.nodes_iter():
+    if n in G_all.nodes():
+        nw = G_all.node[n]['weight']
+        G_kegg.add_node(n, weight=nw)
+    else:
+        G_kegg.add_node(n, weight=0)
+
+for i, j in G_kegg.edges_iter():
+    if (i, j) in G_all.edges():
+        ew = G_all[i][j]['weight']
+        G_kegg.add_edge(i, j, weight=ew)
+    else:
+        G_kegg.add_edge(i, j, weight=0.9)
+
+file_name = '../viz/sub_net_kegg_weighted_all_domain.gexf'
+nx.write_gexf(G_kegg, file_name)
+#-------- END: small kegg network including archaea, bacteria, and eukarya as a network attribute --------#
+
+
+
+#-------- BEGIN: small kegg network including archaea, bacteria, eukarya, and ecosystem as a network attribute --------#
+#-------- END: small kegg network including archaea, bacteria, eukarya, and ecosystem as a network attribute --------#
+
+
+
+
+# #-------- BEGIN: big kegg network including archaea, bacteria, eukarya, ecosystem and small kegg as a network attribute --------#
+# sEdges = bio.sub_edges('biosphere_kegg', 1)
+# G_kegg = nx.Graph(sEdges)
+#
+# for n in G_kegg.nodes_iter():
+#     if n in G_all.nodes():
+#         nw = G_all.node[n]['weight']
+#         G_kegg.add_node(n, weight=nw)
+#     else:
+#         G_kegg.add_node(n, weight=0)
+#
+# for i, j in G_kegg.edges_iter():
+#     if (i, j) in G_all.edges():
+#         ew = G_all[i][j]['weight']
+#         G_kegg.add_edge(i, j, weight=ew)
+#     else:
+#         G_kegg.add_edge(i, j, weight=0.1)
