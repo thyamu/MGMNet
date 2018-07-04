794265ba35b86a4fb8e4dd0104d865c6f064d8e9
hyunju <hyunju@hyunjus-MacBook-Pro.local>
2018-07-04 00:09:00 -0700

    extended analysis for distribution of chiral/achiral in rxn from one set of compounds related to a given rxn to two separate sets: reactants and products

--- code/stat_rxn_chirality_kegg.py
+++ code/stat_rxn_chirality_kegg.py
@@ -35,28 +35,97 @@ rxn_kegg = np.loadtxt(inputfile_name, dtype='str', comments="#", delimiter="\n",
 ##### map each reaction in kegg to chirality of chemical compounds participating in the reaction
 ### array_chiral: chirality distribution \
 kegg = kg.kegg()
-array_chiral = ["reaction", "nbr_total", "nbr_chiral", "nbr_achiral", "nbr_unknown", "ratio_c_to_a", "percentage_c", "percentage_a", "percentage_u"]
+array_chiral = ["reaction", "nbr_total", "nbr_chiral", "nbr_achiral", "nbr_unknown", \
+                "ratio_c_to_a", "percentage_chiral", "percentage_achiral", "percentage_unknown", \
+                "nbr_chiral_reactant", "nbr_chiral_product", \
+                "nbr_achiral_reactant", "nbr_achiral_product", \
+                "nbr_unknown_reactant", "nbr_unknown_product", \
+                "percentage_chiral_reactant", "percentage_chiral_product", \
+                "percentage_achiral_reactant", "percentage_achiral_product", \
+                "percentage_unknown_reactant", "percentage_unknown_product"]
 for x in rxn_kegg:
     if x not in kegg.rxn_prod.keys() or x not in kegg.rxn_reac.keys():
         continue
-    list_com = kegg.rxn_reac[x] + kegg.rxn_prod[x]
+    #list_com = kegg.rxn_reac[x] + kegg.rxn_prod[x]
     nbr_chiral = 0
     nbr_achiral = 0
     nbr_unknown = 0
-    for s in list_com:
+    nbr_chiral_reactant = 0
+    nbr_chiral_product = 0
+    nbr_achiral_reactant = 0
+    nbr_achiral_product = 0
+    nbr_unknown_reactant = 0
+    nbr_unknown_product = 0
+
+    ## To identify chirality of reactants for every reactions x
+    for s in kegg.rxn_reac[x]:
         if dict_chiral[s] == 'chiral':
-            nbr_chiral += 1
+            nbr_chiral_reactant += 1
         elif dict_chiral[s] == 'achiral':
-            nbr_achiral += 1
+            nbr_achiral_reactant += 1
         else:
-            nbr_unknown += 1
-    nbr_total = len(list_com)
+            nbr_unknown_reactant += 1
+
+    ## To compute the percentage of chirality in reactants for reaction x
+    nbr_reactant = len(kegg.rxn_reac[x])
+    percentage_chiral_reactant = 1.0 * nbr_chiral_reactant / nbr_reactant
+    percentage_achiral_reactant = 1.0 * nbr_achiral_reactant / nbr_reactant
+    percentage_unknown_reactant = 1.0 * nbr_unknown_reactant / nbr_reactant
+
+
+    ## To dentify chirality of products for every reactions x
+    for s in kegg.rxn_prod[x]:
+        if dict_chiral[s] == 'chiral':
+            nbr_chiral_product += 1
+        elif dict_chiral[s] == 'achiral':
+            nbr_achiral_product += 1
+        else:
+            nbr_unknown_product += 1
+
+
+    ## To compute the percentage of chirality in products for reaction x
+    nbr_product = len(kegg.rxn_prod[x])
+    percentage_chiral_product = 1.0 * nbr_chiral_product / nbr_product
+    percentage_achiral_product = 1.0 * nbr_achiral_product / nbr_product
+    percentage_unknown_product = 1.0 * nbr_unknown_product / nbr_product
+
+    ## To compute the percentage of chirality in the whole compounds for reaction x
+    nbr_total = nbr_reactant + nbr_product
+    nbr_chiral = nbr_chiral_reactant + nbr_chiral_product
+    nbr_achiral = nbr_achiral_reactant + nbr_achiral_product
+    nbr_unknown = nbr_unknown_reactant + nbr_unknown_product
+
     if nbr_achiral == 0:
         ratio_c_to_a = -1
     else:
         ratio_c_to_a = nbr_chiral * 1.0 / nbr_achiral
-    ac = [x, nbr_total, nbr_chiral, nbr_achiral, nbr_unknown, ratio_c_to_a, \
-        nbr_chiral * 1.0 / nbr_total, nbr_achiral * 1.0 / nbr_total, nbr_unknown * 1.0 / nbr_total]
+
+    percentage_chiral = 1.0 * nbr_chiral / nbr_total
+    percentage_achiral = 1.0 * nbr_achiral / nbr_total
+    percentage_unknown = 1.0 * nbr_unknown / nbr_total
+
+
+
+    '''
+    array_chiral = ["reaction", "nbr_total", "nbr_chiral", "nbr_achiral", "nbr_unknown", \
+                    "ratio_c_to_a", "percentage_chiral", "percentage_achiral", "percentage_unknown", \
+                    "nbr_chiral_reactant", "nbr_chiral_product", \
+                    "nbr_achiral_reactant", "nbr_achiral_product", \
+                    "nbr_unknown_reactant", "nbr_unknown_product", \
+                    "percentage_chiral_reactant", "percentage_chiral_product", \
+                    "percentage_achiral_reactant", "percentage_achiral_product", \
+                    "percentage_unknown_reactant", "percentage_unknown_product"]
+    '''
+
+    ac = [x, nbr_total, nbr_chiral, nbr_achiral, nbr_unknown, \
+        ratio_c_to_a, percentage_chiral, percentage_achiral, percentage_unknown, \
+        nbr_chiral_reactant, nbr_chiral_product, \
+        nbr_achiral_reactant, nbr_achiral_product, \
+        nbr_unknown_reactant, nbr_unknown_product, \
+        percentage_chiral_reactant, percentage_chiral_product, \
+        percentage_achiral_reactant, percentage_achiral_product, \
+        percentage_unknown_reactant, percentage_unknown_product]
+
     array_chiral = np.vstack((array_chiral, ac))
 
 ### path for results
@@ -65,7 +134,7 @@ for ds in ("/chirality", "/stat"):
     dir_output += ds
     if not os.path.exists(dir_output):
         os.makedirs(dir_output)
-output_file_name_dist = dir_output + "/rxn_chiral_distribution_kegg.dat"
+output_file_name_dist = dir_output + "/rxn_chiral_distribution_kegg_new.dat"
 
 ### save the chirality arrays in the output files
 array_chiral.dump(output_file_name_dist)
