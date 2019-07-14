7e914324e9de360c1725a20673ed421ea74ba8ed
Hyunju Kim <hyunju@Hyunjus-MacBook-Pro.local>
2019-07-14 15:06:15 -0700

    change name of file and add script for viz chiarlity on biosphere

--- code/generate_nets.py
+++ code/generate_nets.py
@@ -9,10 +9,11 @@ import mgmnet.topo_measure as tm
 import networkx as nx
 import pickle
 
-module_name = sys.argv[1]
-name = sys.argv[2] # put system name
-s1 = int(sys.argv[3])
-s2 = int(sys.argv[4])
+module_name = sys.argv[1] # put module name (see module_dict.keys())
+name = sys.argv[2] # put system name (eg. individual_archaea_parsed)
+s1 = int(sys.argv[3]) #put species number to begin
+s2 = int(sys.argv[4]) #put species number to end
+t = sys.argv[5] #put type of networks (s for sub_sub; r for rxn_sub)
 module_dict = {'bio': bn.bio(), \
                'union': un.union(), \
                'syn': sn.syn(), \
@@ -20,22 +21,45 @@ module_dict = {'bio': bn.bio(), \
 class_name = module_dict[module_name]
 
 #for system_name in class_name.number_of_species.iterkeys():
-for system_name in [name]:
-    print module_name, system_name
 
-    dr_sub = ''
-    for ds in ('../results', '/networks', '/%s'%(module_name), '/%s'%(system_name)):
-        dr_sub = dr_sub + ds
-        if not os.path.exists(dr_sub):
-            os.makedirs(dr_sub)
+if t == "s":
+    for system_name in [name]:
+        print module_name, system_name
 
-    #for species in range(1, class_name.number_of_species[system_name] + 1):
-    for species in range(s1, s2):
-        print species
-        sEdges = class_name.sub_edges(system_name, species)
-        if len(sEdges) > 0:
-            file_name = dr_sub + '/sub_sub_net_%s-%d'%(system_name, species)
-        G = nx.Graph(sEdges)
-        #with open(file_name, 'wb') as f:
-        #    pickle.dump([G.nodes(data=True), G.edges(data=True)], f)
-        nx.write_gpickle(G, file_name + ".gpickle")
+        dr_sub = ''
+        for ds in ('../results', '/networks', '/%s'%(module_name), '/%s'%(system_name)):
+            dr_sub = dr_sub + ds
+            if not os.path.exists(dr_sub):
+                os.makedirs(dr_sub)
+
+        #for species in range(1, class_name.number_of_species[system_name] + 1):
+        for species in range(s1, s2 + 1):
+            print species
+            sEdges = class_name.sub_edges(system_name, species)
+            if len(sEdges) > 0:
+                file_name = dr_sub + '/sub_sub_net_%s-%d'%(system_name, species)
+            G = nx.Graph(sEdges)
+            #with open(file_name, 'wb') as f:
+            #    pickle.dump([G.nodes(data=True), G.edges(data=True)], f)
+            nx.write_gpickle(G, file_name + ".gpickle")
+
+if t == "r":
+    for system_name in [name]:
+        print module_name, system_name
+
+        dr_rxn = ''
+        for ds in ('../results', '/rxn_networks', '/%s'%(module_name), '/%s'%(system_name)):
+            dr_rxn = dr_rxn + ds
+            if not os.path.exists(dr_rxn):
+                os.makedirs(dr_rxn)
+
+        #for species in range(1, class_name.number_of_species[system_name] + 1):
+        for species in range(s1, s2 + 1):
+            print species
+            rEdges = class_name.rxn_edges(system_name, species)
+            if len(rEdges) > 0:
+                file_name = dr_rxn + '/rxn_sub_net_%s-%d'%(system_name, species)
+            G = nx.Graph(rEdges)
+            #with open(file_name, 'wb') as f:
+            #    pickle.dump([G.nodes(data=True), G.edges(data=True)], f)
+            nx.write_gpickle(G, file_name + ".gpickle")
---
+++ code/viz_chirality_biosphere.py
@@ -0,0 +1,65 @@
+### To generate graphs in gexf format for visulation ###
+#0. biosphere with 1, 2 and 3 embeded
+#1. archaeasphere
+#2. bacteriasphere
+#3. eukaryasphere
+
+### Attributes to add
+# 0. degree (default)
+# 1. domain
+
+import mgmnet.union_nets as un
+import networkx as nx
+
+
+union = un.union()
+system_name = "union_individual_all"
+species = union.number_of_species[system_name]
+print species
+sEdges = union.sub_edges(system_name, species)
+file_name = '../viz/sub_net_%s_chiral'%(system_name)
+G = nx.Graph(sEdges)
+
+##### NOTE: 2019 Version of the file_chirality
+##### UPDATE: Change Name, Path, and Format for the file_chirality
+name_file_chirality =  "/Users/hyunju/research/projects/2019-Chirality/data/kegg/annotation/kegg_chiral_analysis_release.xls"
+
+##### UPDATE 3: Format and Import Method (current: .xls through xlrd)
+import xlrd
+
+book = xlrd.open_workbook(name_file_chirality)
+sh = book.sheet_by_index(1)
+
+
+dict_com_center = dict()
+for s in range(2, sh.nrows):
+    c = sh.cell_value(rowx = s, colx = 0)
+    compound = c.split('.')[0]
+    ch = int(sh.cell_value(rowx = s, colx = 6))
+    dict_com_center[compound] = ch
+
+
+# chiral_centers = []
+# chirality = []
+sum_na = 0
+for n in G.nodes():
+    ##### chiral centers
+    if n not in dict_com_center.keys():
+        sum_na += 1
+        cc = 0
+    else:
+        cc = dict_com_center[n]
+    ##### chirality
+    if cc == 0:
+        chi  = 0
+    else:
+        chi = 1
+    G.add_node(n,center=cc, chirality=chi)
+
+nx.write_gml(G, '%s.gml'%(file_name))
+nx.write_gexf(G, '%s.gexf'%(file_name))
+# print sum_na
+# print len(G.nodes())
+# print G.nodes()["C00001"], G.nodes()["C00006"]
+# print G.nodes()["C00001"]["chirality"]
+# print G.nodes()["C00006"]["center"]
